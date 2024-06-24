c469786057fa32d18766f609eeb4ac4f2c6bd9c3
Chris Woolcott <chris.woolcott@daveramsey.com>
2024-06-24 14:01:15 -0500

    new hand test

--- test/readhand.test.js
+++ test/readhand.test.js
@@ -1,6 +1,6 @@
 const readHand = require("../lib/readhand");
 
-const player = {
+const player1 = {
    id: '35005f48-40da-489a-a00d-dc77856ae5c3',
    name: 'Aggro Alex',
    description: 'A relentless aggressor at the table, Aggro Alex constantly applies pressure to opponents with frequent raises and aggressive betting, often forcing them to fold.',
@@ -68,10 +68,81 @@ const player = {
    allInBet: 75
  };
 
+ const player2 = {
+   id: '723c779b-76f5-4df6-b4c0-432e1be6fab3',
+   name: 'Rogue Ronin',
+   description: 'An enigmatic and unpredictable player, Rogue Ronin defies categorization with a unique blend of aggression, deception, and unorthodox strategies, making him a formidable opponent to face.',
+   aggressionLevel: 7,
+   npc: true,
+   position: 0,
+   money: 600,
+   allIn: false,
+   cards: [
+     {
+       code: '5D',
+       image: 'https://deckofcardsapi.com/static/img/5D.png',
+       images: [Object],
+       value: '5',
+       suit: 'DIAMONDS'
+     },
+     {
+       code: '5C',
+       image: 'https://deckofcardsapi.com/static/img/5C.png',
+       images: [Object],
+       value: '5',
+       suit: 'CLUBS'
+     },
+     {
+       code: 'JD',
+       image: 'https://deckofcardsapi.com/static/img/JD.png',
+       images: [Object],
+       value: 'JACK',
+       suit: 'DIAMONDS'
+     },
+     {
+       code: '7C',
+       image: 'https://deckofcardsapi.com/static/img/7C.png',
+       images: [Object],
+       value: '7',
+       suit: 'CLUBS'
+     },
+     {
+       code: 'JS',
+       image: 'https://deckofcardsapi.com/static/img/JS.png',
+       images: [Object],
+       value: 'JACK',
+       suit: 'SPADES'
+     },
+     {
+       code: '9H',
+       image: 'https://deckofcardsapi.com/static/img/9H.png',
+       images: [Object],
+       value: '9',
+       suit: 'HEARTS'
+     },
+     {
+       code: '5S',
+       image: 'https://deckofcardsapi.com/static/img/5S.png',
+       images: [Object],
+       value: '5',
+       suit: 'SPADES'
+     }
+   ],
+   allInRound: 0,
+   currentBet: 0,
+   potContribution: 0,
+   lastBetRound: 3,
+   cardStrength: [ 'Full House', 7, '5', 'Jack', undefined ],
+   allInBet: 25
+ };
+
 describe("Read Hand", () => {
    test('String should include the word Straight', () => {
-     expect(readHand(player)).toContain('Straight');
+     expect(readHand(player1)).toContain('Straight');
    });
+   test('String should include the word Full Hourse', () => {
+      expect(readHand(player2)).toContain('Full House');
+    });
    test("Should Error with No Valid Cards Sent", () => {
       const cb = () => {
          readHand({})
